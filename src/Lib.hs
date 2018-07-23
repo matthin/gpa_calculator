a63a09e15f94b8c3f5f0610190749c3a63aedbc1
Justin Harrison <me@justinharrison.ca>
2018-07-23 18:29:22 -0400

    Properly calculates a single semester's percentage

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,41 +1,40 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 
 module Lib
     ( someFunc
     ) where
 
-import Text.Printf
-import GHC.Generics
-import Data.Yaml
+import           Data.Yaml
+import           GHC.Generics
+import           Text.Printf
 
-strToGradePoint :: String -> Maybe Float
-strToGradePoint "A+" = Just 4.0
-strToGradePoint "A"  = Just 4.0
-strToGradePoint "A-" = Just 3.7
-strToGradePoint "B+" = Just 3.3
-strToGradePoint "B"  = Just 3.0
-strToGradePoint "B-" = Just 2.7
-strToGradePoint "C+" = Just 2.3
-strToGradePoint "C"  = Just 2.0
-strToGradePoint "C-" = Just 1.7
-strToGradePoint "D+" = Just 1.3
-strToGradePoint "D"  = Just 1.0
-strToGradePoint "D-" = Just 0.7
-strToGradePoint "F"  = Just 0.0
-strToGradePoint _    = Nothing
+data Activity = Activity { title :: String, percentage :: Int } deriving Generic
+instance FromJSON Activity where
 
-data Course = Course { title :: String, grade :: String } deriving Generic
+data Section = Section { title :: String, weight :: Int, activities :: [Activity] } deriving Generic
+instance FromJSON Section where
+
+data Course = Course { title :: String, sections :: [Section] } deriving Generic
 instance FromJSON Course where
 
 someFunc :: IO ()
-someFunc = (decodeFile "example/courses.yml" :: IO (Maybe [Course])) >>= \case
-              Just courses -> case calcGpa (map grade courses) of
-                                Just gpa -> printf "%.2f\n" gpa
-                                Nothing -> putStrLn "Invalid letter grade in courses.yml"
-              Nothing -> putStrLn "courses.yml could not be decoded"
+someFunc = do
+  courses <- decodeFileEither "example/courses.yml" :: IO (Either ParseException [Course])
+  case courses of
+    Left  err     -> print err
+    Right courses -> printf "%d\n" (calcGpa courses)
 
-calcGpa :: [String] -> Maybe Float
-calcGpa grades = case fmap sum . sequence $ map strToGradePoint grades of
-                   Just total -> Just (total / fromIntegral (length grades))
-                   Nothing -> Nothing
+calcGpa :: [Course] -> Int
+calcGpa courses = calcMeanRound (map calcCoursePercentage courses)
+
+calcCoursePercentage :: Course -> Int
+calcCoursePercentage course = sum $ map calcSectionPercentage (sections course)
+
+calcSectionPercentage :: Section -> Int
+calcSectionPercentage section = div
+  (calcMeanRound (map percentage $ activities section) * (weight section))
+  100
+
+calcMeanRound :: [Int] -> Int
+calcMeanRound list = div (sum list) (length list)
